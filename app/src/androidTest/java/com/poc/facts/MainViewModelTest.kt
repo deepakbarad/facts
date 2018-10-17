1567aeb01ba2db7453f8b7f019632c4f692d1a63
Deepak Barad <deepak.barad@wipro.com>
2018-10-17 12:12:46 +0530

    + Included Mockito library + Test to exlcude invalid rows has been added. If the row has no title and description values, then it is considered invalid.

---
+++ app/src/androidTest/java/com/poc/facts/MainViewModelTest.kt
@@ -0,0 +1,49 @@
+package com.poc.facts
+
+import android.app.Application
+import android.support.test.runner.AndroidJUnit4
+import com.poc.facts.models.Fact
+import com.poc.facts.models.FactNews
+import com.poc.facts.viewmodels.MainViewModel
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+
+@RunWith(AndroidJUnit4::class)
+class MainViewModelTest {
+
+    var mainViewModel:MainViewModel? = null;
+    var mockApplication:Application? = null;
+
+    @Before
+    fun setup()
+    {
+        mockApplication = Mockito.mock(Application::class.java)
+        mainViewModel = MainViewModel(mockApplication!!)
+    }
+
+    @Test
+    fun excludeInvalidFactsTest()
+    {
+        var factNews:FactNews = FactNews()
+        var factNewsWithValidFacts = FactNews()
+        var rows:MutableList<Fact> = mutableListOf();
+
+        factNews.title = "Test Data"
+
+        var validFact:Fact = Fact();
+        validFact.title = "ValidFact1"
+        validFact.description = "Valid Fact1 Description"
+        validFact.imageHref = "https://validfact1.com/a.jpg"
+        rows.add(validFact)
+
+        var invalidFact:Fact = Fact()
+        rows.add(invalidFact)
+
+        factNews.rows = rows;
+        factNewsWithValidFacts = mainViewModel!!.getValidFacts(factNews)
+
+        assert(factNewsWithValidFacts.rows.size == 1);
+    }
+}
